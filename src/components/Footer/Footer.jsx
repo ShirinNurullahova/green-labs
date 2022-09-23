c9d325b36cccddcb5b008631f1eba7837af7d792
ShirinNurullahova <90415981+ShirinNurullahova@users.noreply.github.com>
2022-09-23 14:01:57 +0400

    svg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,18 @@ const Footer = forwardRef((props, ref) => {
 
   return (
     <div className='footerEsas' ref={ref}>
+      <div class="brand-side-logo">
+            <svg viewBox="0 0 249 696.6">
+                <path fill='#0bd46e' d="M8.6,202.5L8.6,202.5c-17.3-31.4-5.9-70.8,25.5-88.1l0,0L240.4,0.7l0,0c17.3,31.4,5.9,70.8-25.5,88.1l0,0 L8.6,202.5z"></path>
+                <path   stroke="#0bd46e" fill='transparent' d="M9.1,285.4L9.1,285.4c-14.2-25.7-4.9-57.9,20.8-72.1l0,0l168.7-93l0,0c14.1,25.7,4.8,57.9-20.8,72.1l0,0 L9.1,285.4z"></path>
+                <path stroke="#0bd46e" fill='transparent' d="M11.5,358.8L11.5,358.8c-12.9-23.3-4.4-52.6,18.9-65.5l0,0l153.3-84.5l0,0c12.9,23.3,4.4,52.6-18.9,65.5l0,0 L11.5,358.8z"></path>
+                <path opacity='0.6' stroke="#0bd46e" fill='transparent' d="M14,432.2L14,432.2c-11.6-21-3.9-47.3,17-58.9l0,0l137.9-76l0,0c11.6,21,3.9,47.3-17,58.9L14,432.2z"></path>
+                <path opacity='0.5' stroke="#0bd46e" fill='transparent' d="M16.5,505.7L16.5,505.7c-10.3-18.6-3.5-42,15.1-52.3L154,385.9l0,0c10.3,18.6,3.5,42-15.1,52.3L16.5,505.7z"></path>
+                <path opacity='0.4' stroke="#0bd46e" fill='transparent' d="M19,579.1L19,579.1c-9-16.3-3.1-36.7,13.2-45.7l0,0l107-59l0,0c9,16.3,3.1,36.7-13.2,45.7L19,579.1z"></path>
+                <path opacity='0.3' stroke="#0bd46e" fill='transparent' d="M21.5,642.5L21.5,642.5c-7.7-13.9-2.6-31.4,11.3-39.1l91.6-50.5l0,0c7.7,13.9,2.6,31.4-11.3,39.1L21.5,642.5z"></path>
+                <path opacity='0.2' stroke="#0bd46e" fill='transparent' d="M24,695.9L24,695.9c-6.4-11.6-2.2-26.1,9.4-32.5l76.2-42l0,0c6.4,11.6,2.2,26.1-9.4,32.5l0,0L24,695.9z"></path>
+            </svg>
+        </div>
       <div className='footer-up'>
         <div className='connect'>CONNECT</div>
         <div>
